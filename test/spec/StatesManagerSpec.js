aa77d520ed6ea63e98756896f594afcaf55b4849
Chriztian Steinmeier <chriztian@steinmeier.dk>
2021-12-03 22:13:11 +0100

    Rewrite Spec to match the new design

--- test/spec/StatesManagerSpec.js
+++ test/spec/StatesManagerSpec.js
@@ -1,56 +1,43 @@
 import StatesManager from '../../src/states-manager.js'
 
 describe('StatesManager', function() {
-	it('can be initialized with a single item & state', function() {
-		const manager = new StatesManager({}, 'on')
+	fit('can be initialized with a CSS selector', () => {
+		const mngr = new StatesManager('.menubar')
 		
-		expect(manager.items.length).toBe(1)
-		expect(manager.states.length).toBe(2) // 
-		expect(manager.states).toEqual([null, 'on'])
+		expect(mngr.element).toBeDefined()
 	})
 	
-	it('can be initialized with a single item & multiple states', function() {
-		const manager = new StatesManager([{}], ['on', 'disabled', 'zoned'])
+	it('can be initialized wuith a DOM element', () => {
 		
-		expect(manager.items.length).toBe(1)
-		expect(manager.states.length).toEqual(4)
-		expect(manager.states).toEqual([null, 'on', 'disabled', 'zoned'])
 	})
 	
-	it('can be initialized with multiple items & a single state', function() {
-		const manager = new StatesManager([{}, {}, {}], 'disabled')
+	it('detects an existing state on the DOM element', () => {
 		
-		expect(manager.items.length).toBe(3)
-		expect(manager.states.length).toEqual(2)
-		expect(manager.states).toEqual([null, 'disabled'])
 	})
 	
-	describe('accessing item state', function() {
-		let testItems
-		let testStates
-		let manager
+	it('detects any existing modifiers set with a data-modifiers attribute', () => {
 		
-		beforeEach(function() {
-			testItems = [ { name: 'One' }, { name: 'Two' }]
-			testStates = ['selected', 'disabled']
-			manager = new StatesManager(testItems, testStates)
-		})
+	})
+	
+	it('detects any existing modifiers set with CSS classes', () => {
 		
-		describe('setStateForItem', function() {
+	})
+	
+	describe('setState', () => {
+		it('can set state by index', () => {
 			
-			it('can change the state of an item by its index', function() {
-				
-				manager.setStateForItem(1, 0)
-				expect(manager.items[1].state).toBe(0)
-			})
 		})
 		
-		describe('getStateForItem', function() {
-			it("it reports an item's current state", function() {
-				expect(manager.getStateForItem(1)).toBeNull()
-				manager.setStateForItem(1, 1)
-				expect(manager.getStateForItem(1)).toBe(1)
-			})
+		it('detects an out of range index and doesn\'t apply a state', () => {
+			
+		});
+		
+		it('can set state by string', () => {
+			
 		})
+		
+		it('does not apply the state if it isn\'t one of the registered options', () => {
+			
+		});
 	})
 })
